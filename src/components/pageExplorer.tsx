15a559a2fc3301849dd04e218135c12b59cff785
hicode <15054613+everskyblue@users.noreply.github.com>
2024-08-02 11:33:53 -0500

    añadido permiso de almacenamiento

--- src/components/pageExplorer.tsx
+++ src/components/pageExplorer.tsx
@@ -26,7 +26,7 @@ const permission_storage = [
     'android.permission.READ_EXTERNAL_STORAGE',
     'android.permission.WRITE_EXTERNAL_STORAGE',
     'android.permission.INTERNET',
-    //'android.permission.MANAGE_EXTERNAL_STORAGE'
+    'android.permission.MANAGE_EXTERNAL_STORAGE'
 ]
 
 const refreshCollection = (path: string = currentDirectory) => {
