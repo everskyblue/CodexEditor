75d768881336f94b06c2a9295eeef8c6293339bd
hicodx <hicodx@gmail.com>
2024-08-19 16:38:26 -0500

    añadido el componente ActivityBar

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -8,13 +8,19 @@ import {
     Action,
     drawer,
     ScrollView,
+    devTools,
+    TextView
 } from "tabris";
-import addView, { CoordinatePage } from "voir-native";
+import { addView, CoordinatePage } from "voir-native";
 import FileExplore from "./ui/FileExplorer";
 import SideView from "./ui/SideView";
 import TabView from "./components/TabView";
 import { resolve, basename } from 'path'
 import { getStorage } from "./storage";
+import { ActivityBar, ActivityBarLayout } from './components/ActivityBar'
+import { ExtensionView, FilterView } from './components/ExtensionView'
+
+//devTools.hideUi();
 
 contentView.append(
     <CoordinatePage toolbarColor="#000022" layoutData="stretch" drawerActionVisible>
@@ -43,11 +49,45 @@ const side = async () => {
     const views = await SideView();
     const dirname = basename(getStorage().currentProject);
     drawer.background = '#312c4a';
-    drawer.append(
+    /*drawer.append(
         <TabView title={dirname}>
             {views}
         </TabView>
-    );
+    );*/
+    drawer.append(
+        <ActivityBar open={1}>
+            <ActivityBarLayout 
+                title='proyecto'
+                image='/assets/img/file.png'
+            >
+                <TabView title={dirname}>
+                    <ScrollView
+                        left={0}
+                        right={0}
+                        baseline
+                        //id='interactiveHScrollContent'
+                        class='interactiveHScrollContent'
+                        direction="horizontal"
+                        padding={5}
+                    >
+                        {views}
+                    </ScrollView>
+                </TabView>
+            </ActivityBarLayout>
+            <ActivityBarLayout 
+                title='extensiones'
+                image='/assets/img/packages.png'
+            >
+                <TextView text='extensiones' padding={8} textColor='gray' />
+                <FilterView />
+                <TabView title='extensiones instaladas' top='prev() 15'>
+                    <ExtensionView />
+                </TabView>
+            </ActivityBarLayout>
+        </ActivityBar>
+    )
+    
+    drawer.open()
 };
 
 side();
