23b3781b63539d8599c4be8200a21c7a59939073
hicodx <hicodx@gmail.com>
2024-08-23 01:10:35 -0500

    se añadio mas acciones

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -10,7 +10,9 @@ import {
     drawer,
     ScrollView,
     devTools,
-    TextView
+    TextView,
+    Stack,
+    WebView
 } from "tabris";
 import { addView, CoordinatePage } from "voir-native";
 import FileExplore from "./ui/FileExplorer";
@@ -21,6 +23,10 @@ import { getStorage } from "./storage";
 import { ActivityBar, ActivityBarLayout } from "./components/ActivityBar";
 import { ExtensionView, FilterView } from "./components/ExtensionView";
 import { theme } from './theme'
+import viewSetting from './action-view/settings'
+import actionOpenFile from './action-view/openFile'
+import actionCreateProject from './action-view/createProject'
+import actionShowProjects from './action-view/showProjects'
 //devTools.hideUi(); background="#312c4a" 
 
 contentView.append(
@@ -29,9 +35,66 @@ contentView.append(
         layoutData="stretch"
         drawerActionVisible
     >
-        <Action title="explorar" placement="overflow" onSelect={FileExplore} />
+        <Action
+            title="Abrir Archivo"
+            placement="overflow"
+            onSelect={actionOpenFile}
+        />
+
+        <Action
+            title="Crear Proyecto"
+            placement="overflow"
+            onSelect={actionCreateProject}
+        />
+
+        <Action
+            title="Proyectos Abiertos"
+            placement="overflow"
+            onSelect={actionShowProjects}
+        />
+        
+        <Action 
+            title="explorar" 
+            placement="overflow"
+            onSelect={FileExplore} 
+        />
+        
+        <Action 
+            title="configuracion" 
+            placement="overflow" 
+            onSelect={viewSetting} 
+        />
+        
+        <Action
+            title="Salir"
+            placement="overflow"
+            onSelect={() => app.close()}
+        />
+        
         <Page title="CodexEditor">
-            <Composite id="container" stretch></Composite>
+            <Stack
+                stretch
+            >
+                <ScrollView
+                    id="tab-editor"
+                    stretchX
+                    background={theme.Tab.background()}
+                >
+                    <TextView
+                        highlightOnTouch
+                        text="my tab"
+                        width={100}
+                        background={theme.Tab.activeBackground()}
+                        textColor={theme.Tab.foreground()}
+                        padding={[8, 16]}
+                    />
+                </ScrollView>
+                <WebView
+                    stretch
+                    url="/editor.html"
+                />
+                <Composite id="container" stretch></Composite>
+            </Stack>
         </Page>
     </CoordinatePage>
 );
@@ -48,16 +111,16 @@ const codex = new Codex($("#container").only(), {
     }
 });
 
-codex.create();
+//codex.create();
 
 const side = async () => {
     drawer.background = theme.SideBar.background(drawer);
     const views = await SideView();
     const dirname = basename(getStorage().currentProject);
     drawer.append(
-        <ActivityBar open={1}>
+        <ActivityBar open={0}>
             <ActivityBarLayout title="proyecto" image="/assets/img/file.png">
-                <TabView title={dirname}>
+                <TabView title={dirname} id="view-project">
                     <ScrollView
                         left={0}
                         right={0}
@@ -78,8 +141,6 @@ const side = async () => {
             </ActivityBarLayout>
         </ActivityBar>
     );
-
-    drawer.open();
 };
 
 side();
