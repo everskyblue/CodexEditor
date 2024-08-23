82d61b3a5ebc82225d5eacca45ef8488a08d72ca
hicodx <hicodx@gmail.com>
2024-08-23 17:28:45 -0500

    se mejoro la logica de los tabs y de implemento la accion de los tabs para que cambie los colores

--- project/src/components/tabs/TabCode.js
+++ project/src/components/tabs/TabCode.js
@@ -1,19 +1,36 @@
 import { TextView, WebView } from "tabris";
 import { theme } from "../../theme";
+import libs from '../../libraries'
 
+let tabId = 0;
 
-export function TabCode({title, url}) {
+function loadWebView(wv, file) {
+    //readFile(file)
+}
+
+export function TabCode({title, file, url = '/editor.html'}) {
+    const textId = `tab-${++tabId}`;
+    const wvId = `tab-ref-${tabId}`;
     return (
         <$>
+            <WebView 
+                stretch 
+                id={wvId} 
+                class={textId} 
+                url={url}
+                onLoad={({target})=> loadWebView(target, file)}
+            />
             <TextView 
                 highlightOnTouch
                 text={title} 
+                left='prev()'
                 width={100}
+                data={{active: true}}
                 background={theme.Tab.activeBackground()}
                 textColor={theme.Tab.foreground()}
                 padding={[8, 16]}
+                id={textId}
             />
-            <WebView stretch url={url} />
         </$>
     )
 }
--- project/src/components/tabs/TabEditor.tsx
+++ project/src/components/tabs/TabEditor.tsx
@@ -1,18 +1,70 @@
 import { theme } from "../../theme";
-import { ScrollView, Stack } from "tabris";
+import { ScrollView, TextView, WebView, Composite, Stack, CompositeAddChildEvent } from "tabris";
 
+export * from './TabCode'
+
+class TabContent extends Composite {
+    constructor() {
+        super({
+            layoutData: 'stretch'
+        })
+    }
+}
 
 export class TabEditor extends Stack {
+    addChildEvent = ({child: tabLink}: CompositeAddChildEvent) => {
+        tabLink.siblings(TextView).set({
+            data: { visible: false },
+            // añadir bg inactivo
+            background: theme.Tab.inactiveBackground(),
+            textColor: theme.Tab.inactiveForeground()
+        })
+
+        // añadir bg activo
+       tabLink.background = theme.Tab.activeBackground();
+       //@ts-ignore
+       tabLink.textColor = theme.Tab.foreground();
+        
+        const currentContent = (this.children()[1] as TabContent)
+            .find(`.${tabLink.id}`)
+            .first();
+
+        currentContent.visible = true;
+        currentContent.siblings().set({
+            visible: false
+        });
+    }
+    
+    set widgetTitle(widget: TextView) {
+        widget.on('tap', () => {
+            this.addChildEvent({child: widget} as unknown as CompositeAddChildEvent);
+        })
+    }
+
     constructor() {
         super({
             layoutData: 'stretch'
         })
 
         this.append(
-            <ScrollView id="tab-editor" 
+            <ScrollView id="tab-scroll-editor" 
                 stretchX
+                direction='horizontal'
                 background={theme.Tab.background()} 
-            />
+                onAddChild={this.addChildEvent}
+            />,
+            <TabContent />
         )
+    }
+
+    _addChild(child: any, index: number) {
+        if (child instanceof ScrollView || child instanceof TabContent) {
+            super._addChild(child, index);
+        } else if (child instanceof TextView) {
+            this.widgetTitle = child;
+            this._find(ScrollView).first().append(child)
+        } else if (child instanceof WebView) {
+            this._find(TabContent).first().append(child);
+        }
     }
 }
